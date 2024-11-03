7048a24e0a25d6fd007c6422bbbef0a5dd41f9f7
Rifai-13 <130067475+Rifai-13@users.noreply.github.com>
2024-11-03 10:01:53 +0700

    Updatehomeview

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,175 +8,293 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 300,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-                'assets/bg_home.png'), // Ganti dengan path gambar Anda
-            fit: BoxFit.cover,
-          ),
-        ),
+      body: SingleChildScrollView( // Membungkus dengan SingleChildScrollView
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            AppBar(
-              backgroundColor: Colors.black.withOpacity(
-                  0.5), // Mengatur warna latar belakang AppBar dengan transparansi
-              title: Row(
+            // Background atas dengan gambar
+            Container(
+              height: 270,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/bg_home.png'), // Path gambar Anda
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 180, // Ubah lebar sesuai kebutuhan
-                    child: TextField(
-                      decoration: InputDecoration(
-                        isDense: true, // Mengatur agar TextField lebih kecil
-                        prefixIcon: const Icon(Icons.search),
-                        hintText: 'Search...',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
+                  AppBar(
+                    backgroundColor: Colors.black.withOpacity(0.5),
+                    title: Row(
+                      children: [
+                        Container(
+                          width: 180,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              isDense: true,
+                              prefixIcon: const Icon(Icons.search),
+                              hintText: 'Search...',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 8),
+                            ),
+                          ),
                         ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 4, horizontal: 8), // Padding lebih kecil
-                      ),
+                        IconButton(
+                          icon: const Icon(Icons.notifications,
+                              color: Colors.white),
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.shopping_cart,
+                              color: Colors.white),
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.person, color: Colors.white),
+                          onPressed: () {},
+                        ),
+                      ],
                     ),
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.notifications, color: Colors.white),
-                    onPressed: () {},
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16, top: 16),
+                    child: Text(
+                      'Hai!',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.shopping_cart, color: Colors.white),
-                    onPressed: () {},
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16, top: 8),
+                    child: Text(
+                      'Selamat Datang di Gizmogate',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.person, color: Colors.white),
-                    onPressed: () {},
+                  const SizedBox(height: 16), // Spasi antara teks dan tombol
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[800],
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 4),
+                              minimumSize: const Size(80, 30),
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.black,
+                              textStyle: const TextStyle(fontSize: 12),
+                            ),
+                            child: const Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.upload, size: 16),
+                                SizedBox(width: 4),
+                                Text('Upload Produk'),
+                              ],
+                            ),
+                          ),
+                          const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.edit, color: Colors.white, size: 16),
+                                  SizedBox(width: 4),
+                                  Text("Status Produk",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 10)),
+                                ],
+                              ),
+                              SizedBox(width: 8),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.sell, color: Colors.white, size: 16),
+                                  SizedBox(width: 4),
+                                  Text("Penjualan",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 10)),
+                                ],
+                              ),
+                              SizedBox(width: 8),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.shopping_cart,
+                                      color: Colors.white, size: 16),
+                                  SizedBox(width: 4),
+                                  Text("Pembelian",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 10)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            Expanded(
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 16, top: 16),
-                      child: Text(
-                        'Hai!',
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
+            // Grid untuk slider
+            Container(
+              height: 150, // Atur tinggi sesuai kebutuhan Anda
+              child: PageView.builder(
+                itemCount: 3,
+                onPageChanged: (index) {
+                  controller.updateSliderIndex(index);
+                },
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey[700],
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 16, top: 8),
-                      child: Text(
-                        'Selamat Datang di Gizmogate',
-                        style: TextStyle(
-                            fontSize: 16,
+                      child: Center(
+                        child: Text(
+                          'Slide ${index + 1}',
+                          style: const TextStyle(
+                            fontSize: 18,
                             fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[800],
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 5,
-                              offset: Offset(0, 2),
-                            ),
-                          ],
-                        ),
-                        padding: const EdgeInsets.all(8),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ElevatedButton.styleFrom(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8, vertical: 4),
-                                    minimumSize: const Size(80, 30),
-                                    backgroundColor: Colors.white,
-                                    foregroundColor: Colors.black,
-                                    textStyle: const TextStyle(fontSize: 12),
-                                  ),
-                                  child: const Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Icon(Icons.upload, size: 16),
-                                      SizedBox(width: 4),
-                                      Text('Upload Produk'),
-                                    ],
-                                  ),
-                                ),
-                                const Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Icon(Icons.edit,
-                                            color: Colors.white, size: 16),
-                                        SizedBox(width: 4),
-                                        Text("Status Produk",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10)),
-                                      ],
-                                    ),
-                                    SizedBox(width: 8),
-                                    Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Icon(Icons.sell,
-                                            color: Colors.white, size: 16),
-                                        SizedBox(width: 4),
-                                        Text("Penjualan",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10)),
-                                      ],
-                                    ),
-                                    SizedBox(width: 8),
-                                    Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Icon(Icons.shopping_cart,
-                                            color: Colors.white, size: 16),
-                                        SizedBox(width: 4),
-                                        Text("Pembelian",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10)),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 8),
-                          ],
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
+              ),
+            ),
+            // Indicator bar
+            Obx(() {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(3, (index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 4, vertical: 8),
+                    width: 8,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: controller.sliderIndex == index
+                          ? Colors.blueGrey
+                          : Colors.grey,
+                    ),
+                  );
+                }),
+              );
+            }),
+            // Baris untuk Kategori Produk
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0), // Menghapus padding vertikal
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Kategori Produk',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Aksi untuk 'See All'
+                    },
+                    child: const Text(
+                      'See All',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Grid untuk kategori produk
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+                children: [
+                  _buildCategoryBox('Mouse'),
+                  _buildCategoryBox('Keyboard'),
+                  _buildCategoryBox('Monitor'),
+                  _buildCategoryBox('Laptop'),
+                  _buildCategoryBox('Handphone'),
+                  _buildCategoryBox('Headset'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryBox(String name) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blueGrey[500],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
             ),
           ],
